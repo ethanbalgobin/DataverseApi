12f228d49e3edd76c2c4a955c9514a96a6a911c4
ethanbalgobin <ethanbalgo@hotmail.com>
2026-03-08 21:35:42 +0000

    add PATCH /contact and cache JsonSerializerOptions

--- Models/DataverseSettings.cs
+++ Models/DataverseSettings.cs
@@ -1,9 +1,9 @@
 ﻿namespace DataverseAPI.Models;
 
 public class DataverseSettings
-    {
-        public string DataverseUrl { get; set; } = string.Empty;
-        public string ClientId { get; set; } = string.Empty;
-        public string ClientSecret { get; set; } = string.Empty;
-            public string TenantId { get; set; } = string.Empty;
-        }
+{
+    public string DataverseUrl { get; set; } = string.Empty;
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+    public string TenantId { get; set; } = string.Empty;
+}
